5fe58e0a870f2786ce7728bf5745312331cc0594
Neshiga R <neshigaravi@gmail.com>
2026-08-13 21:21:16 +0530

    Complete MERN ecommerce project

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,21 +1,31 @@
 import jwt from 'jsonwebtoken';
 
 const authUser = (req, res, next) => {
-     
-    const {token} = req.headers;
 
-    if(!token){
-        return res.json({success:false,message:'Not Authorized Login Again'})
+    const { token } = req.headers;
+
+    if (!token) {
+        return res.json({
+            success: false,
+            message: 'Not Authorized Login Again'
+        });
     }
 
     try {
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId=token_decode.id;
+
+        req.body = req.body || {};
+        req.body.userId = token_decode.id;
+
         next();
-       
+
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:error.message})
+
+        res.json({
+            success: false,
+            message: error.message
+        });
     }
 }
 

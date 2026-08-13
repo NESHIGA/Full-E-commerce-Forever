452970b05048996363fe1f154466533f3ebed234
Neshiga R <neshigaravi@gmail.com>
2026-08-13 12:36:42 +0530

    Complete MERN ecommerce project

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,18 @@ connectDB()
 connectCloudinary()
 //middlewares
 app.use(express.json())
-app.use(cors())
+const allowedOrigins = [
+    'https://backend-full-e-commerce-forever.vercel.app',  // Frontend URL
+    'http://localhost:5173',   // Local Vite
+    'http://localhost:3000',   // Local React
+    'http://localhost:4000'    // Local Backend
+];
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 // api endpoints
 app.use('/api/user',userRouter)
